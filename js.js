6287fcfe864351c42caf55f148566ef8d2109480
Alana <ribeiroalana2005@gmail.com>
2022-08-16 15:33:18 -0300

    changed everything

--- js.js
+++ js.js
@@ -1,30 +1,24 @@
-/* open and close menu */
-const nav = document.querySelector("#header nav")
-const toggle = document.querySelectorAll("nav .toggle")
-
-for (const element of toggle) {
-    element.addEventListener('click', function () {
-        nav.classList.toggle('show')
-    })
-}
-
-/* when you click the item, close menu */
-const links = document.querySelectorAll('nav ul li a')
-
-for (const link of links) {
-    link.addEventListener('click', function () {
-        nav.classList.remove('show')
-    })
-}
-
-/* changed header scroll */
+// changed header scroll 
 const header = document.querySelector('header')
 const navHeight = header.offsetHeight
 
 window.addEventListener('scroll', function () {
-   if(window.scrollY >= navHeight) {   
-    header.classList.add('scroll')
+    if (window.scrollY >= navHeight) {
+        header.classList.add('scroll')
     } else {
-    header.classList.remove('scroll')
+        header.classList.remove('scroll')
     }
 })
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: false
+})
+
+scrollReveal.reveal(
+    `#home .title, #home .linkHome,
+    #aboutMe .text, #contact .contacts,
+    #projects .title, #projects .card`
+)
